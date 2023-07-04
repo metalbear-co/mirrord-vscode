61371c49fe30b644ee3d83ea24fb10c87631ed3a
Eyal Bukchin <eyal@metalbear.co>
2023-07-04 18:20:07 +0300

    Lower timeout

--- src/api.ts
+++ src/api.ts
@@ -175,7 +175,7 @@ export class MirrordAPI {
             return new Promise<MirrordExecution>((resolve, reject) => {
                 setTimeout(() => {
                     reject("timeout");
-                }, 600 * 1000);
+                }, 120 * 1000);
 
                 const args = ["ext"];
                 if (target) {
