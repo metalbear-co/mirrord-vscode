247713abe654f4f3bf9ab317ba8e94ceb4c43fba
Michał Smolarek <34063647+Razz4780@users.noreply.github.com>
2023-08-10 15:44:28 +0200

    Version mismatch warn (#51)
    * Disabling agent version mismatch warning
    
    * Changelog entry

--- src/api.ts
+++ src/api.ts
@@ -298,6 +298,8 @@ export class MirrordAPI {
           }
         });
 
+        const warningHandler = new MirrordWarningHandler();
+
         let buffer = "";
         child.stdout.on("data", (data) => {
           console.log(`mirrord: ${data}`);
@@ -328,9 +330,7 @@ export class MirrordAPI {
             }
 
             if (message["type"] === "Warning") {
-              new NotificationBuilder()
-                .withMessage(message["message"])
-                .warning();
+              warningHandler.handle(message["message"]);
             } else {
               // If it is not last message, it is progress
               let formattedMessage = message["name"];
@@ -346,6 +346,30 @@ export class MirrordAPI {
   }
 }
 
+class MirrordWarningHandler {
+  private filters: [(message: string) => boolean, string][];
+
+  constructor() {
+    this.filters = [
+      [
+        (message: string) => message.includes("Agent version") && message.includes("does not match the local mirrord version"),
+        "promptAgentVersionMismatch",
+      ]
+    ];
+  }
+
+  handle(warningMessage: string) {
+    const builder = new NotificationBuilder()
+      .withMessage(warningMessage);
+
+    const filter = this.filters.find(filter => filter[0](warningMessage));
+    if (filter !== undefined) {
+      builder.withDisableAction(filter[1]);
+    }
+
+    builder.warning();
+  }
+}
 
 /** 
 * Updates the global feedback counter. When it hits `FEEDBACK_COUNTER_REVIEW_AFTER` mirrord runs, 
