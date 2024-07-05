bb982d6becbd477f32b88a57f1362529d5bb7275
t4lz <t4lz.git@gmail.com>
2024-07-05 17:47:35 +0200

    support debugpy on macos (#133)
    * support debugpy
    
    * add best effort support for unkown python configs
    
    * but only if file exists

--- src/debugger.ts
+++ src/debugger.ts
@@ -7,6 +7,7 @@ import { getMirrordBinary } from './binaryManager';
 import { platform } from 'node:os';
 import { NotificationBuilder } from './notification';
 import { setOperatorUsed } from './mirrordForTeams';
+import fs from 'fs';
 
 const DYLD_ENV_VAR_NAME = "DYLD_INSERT_LIBRARIES";
 
@@ -27,6 +28,7 @@ function getFieldAndExecutable(config: vscode.DebugConfiguration): [keyof vscode
       // for SIP, so we pass the shell to the mirrod CLI.
       return ["command", vscode.env.shell];
     }
+    case "debugpy":
     case "python": {
       if ("python" in config) {
         return ["python", config["python"]];
@@ -36,6 +38,11 @@ function getFieldAndExecutable(config: vscode.DebugConfiguration): [keyof vscode
       return ["pythonPath", config["pythonPath"]];
     }
     default: {
+      if ("python" in config && fs.existsSync(config["python"])) {
+        // We don't know that config type yet, but the config has the field "python" that contains a path that exists,
+        // so we assume that's the path of the python binary and we patch that.
+        return ["python", config["python"]];
+      }
       return ["program", config["program"]];
     }
   }
